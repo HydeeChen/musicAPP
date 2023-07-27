610b6663e19e51eff47d322508e251e8ba221dd3
hydee <hydee0209@hotmail.com>
2023-07-27 22:47:20 +0800

    Initial Commit

---
+++ 音樂播放器/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  音樂播放器
+//
+//  Created by Hydee Chen on 2023/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
